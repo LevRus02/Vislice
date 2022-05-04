b2207d1218e6dda8653de12bc0662e8e147fb21e
Lev Rus <lr3979@student.uni-lj.si>
2022-05-04 15:15:43 +0200

    Drugi Commit

---
+++ model.py
@@ -0,0 +1,10 @@
+stevilo_dovoljenih_napak = 10
+pravilna_crka = '+'
+ponovljena_crka = 'o'
+napacna_crka = '-'
+zmaga = 'W'
+poraz = 'X'
+
+class Igra:
+    def __init__(self):
+        zmaga.self = 'geslo'
